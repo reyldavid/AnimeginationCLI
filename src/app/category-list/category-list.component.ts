1054d1991bb5a1c5450b4adc9f9ce01bc6ed87ee
Rey David <reydavid47@gmail.com>
2018-11-17 15:22:30 -0800

    Categories List component

--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CategoryService } from '../services/categories.service';
+import { Category } from '../models/category';
+import { ApiProduct } from '../models/product';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  products: ApiProduct[];
+  category: string;
 
-  ngOnInit() {
+  constructor(private _router: Router, 
+      private _route: ActivatedRoute, 
+      private _categoryService: CategoryService ) {
+   }
+
+  ngOnInit(): any {
+    console.log('category list init');
+
+    this._route.queryParams.subscribe(params => {
+      let categoryID = params.categoryID;
+
+      console.log('aya categoryID');
+      console.log(categoryID);
+
+      this.GetCategory(categoryID);
+      this.GetProducts(categoryID);
+    })
   }
 
+  OnSelectProduct(product: ApiProduct) {
+    console.log('category product ID: ' + product.ProductID);
+    this._router.navigate(['/detail'], { queryParams: { productID: product.ProductID } });
+  }
+
+  GetCategory(categoryID: number) {
+      this._categoryService.getAnimeCategory(categoryID)
+          .subscribe(
+            (category: Category) => {
+              this.category = category.Description;              
+            });
+  }
+
+  GetProducts(categoryId: number) {
+    this._categoryService.getAnimeCategoryList(categoryId)
+      .subscribe(
+        (products: ApiProduct[]) => {
+          this.products = products;              
+      });
+  }
 }
