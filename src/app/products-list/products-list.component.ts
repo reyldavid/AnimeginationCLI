cab9939e50adb6279a504ab8565021a7182d1e01
Rey David <reydavid47@gmail.com>
2018-11-18 17:02:30 -0800

    Products by List Type

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiProduct } from '../models/product';
+import { ListType } from '../models/listtype';
+import { ListingService } from '../services/listings.service';
+import { ListTypeService } from '../services/listtypes.service';
 
 @Component({
   selector: 'app-products-list',
@@ -7,9 +12,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsListComponent implements OnInit {
 
-  constructor() { }
+  products: ApiProduct[];
+  listType: ListType;
+  title: string;
 
-  ngOnInit() {
+  constructor(private _router: Router, 
+      private _route: ActivatedRoute, 
+      private _listingService: ListingService, 
+      private _listTypeService: ListTypeService ) {
+   }
+
+  ngOnInit(): any {
+    console.log('products list init');
+
+    this._route.queryParams.subscribe(params => {
+      let listTypeId = params.listTypeId;
+
+      console.log('aya listTypeId');
+      console.log(listTypeId);
+
+      this.GetProductListType(listTypeId);
+      this.GetProducts(listTypeId);
+    })
+
+    this._route.paramMap.subscribe(params => {
+      let listTypeIdparam = params.get('listTypeId');
+  
+      if (listTypeIdparam) {
+        let typeID = Number(listTypeIdparam) % 10;
+
+        this.GetProductListType(typeID);
+        this.GetProducts(typeID);
+      }
+    })
+  }
+  
+  GetProducts(listTypeId: number) {
+    this._listingService.getAnimeListing(listTypeId)
+        .subscribe((apiProducts: ApiProduct[]) => {
+          this.products = apiProducts;
+  
+          this.GetProductListType(listTypeId);
+        });
+  }
+
+  GetProductListType(listTypeId: number) {
+    this._listTypeService.getAnimeListType(listTypeId)
+        .subscribe((listType: ListType) => {
+          this.listType = listType;
+          this.title = listType.Description;
+        });
+  }
+  
+  OnSelectProduct(product: ApiProduct) {
+    console.log('list type product ID: ' + product.ProductID);
+    this._router.navigate(['/detail'], { queryParams: { productID: product.ProductID } });
   }
 
 }
+
