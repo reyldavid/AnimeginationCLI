59669057e0f9a55a7dc54bb95c2a92cfe06e032a
Rey David <reydavid47@gmail.com>
2018-11-18 17:03:18 -0800

    Products Footer

---
+++ src/app/footer-product/footer-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-product',
+  templateUrl: './footer-product.component.html',
+  styleUrls: ['./footer-product.component.css']
+})
+export class FooterProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
