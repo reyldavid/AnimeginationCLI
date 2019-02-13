7d33d805546edf9d28e724e66e4c3d0bae95b91c
Rey David <reydavid47@gmail.com>
2019-02-12 20:13:24 -0800

    new Cart Item

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
+import { CartItem } from '../models/cartItemModel';
+import { MessageService } from '../services/message.service';
+import { SessionService } from '../services/session.service';
+import { Subscription } from 'rxjs/Subscription';
+import { OrderService } from '../services/orders.service';
 
 @Component({
   selector: 'app-cart-item',
@@ -7,9 +14,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() 
+  cartItem: Product;
+
+  constructor( private router: Router, 
+               private route: ActivatedRoute, 
+               private messageService: MessageService,
+               private sessionService: SessionService,  
+               private orderService: OrderService ) { 
+    }
 
   ngOnInit() {
   }
 
+  OnSelectProduct(cartItem: CartItem) {
+    console.log('product slide product ID: ' + cartItem.productID);
+    // this.router.navigate(['/detail', { productID: cartItem.productID }]);
+    this.router.navigate(['/detail'], { queryParams: {  productID: cartItem.productID } });
+  }
+
+  updateQuantity(quantity, cartItem) {
+    console.log("aya " + quantity);
+    console.log("ueto " + cartItem.orderID);
+
+      this.orderService.getOrderItemById(this.sessionService.UserToken, cartItem.orderItemID)
+        .subscribe( orderItem => {
+          console.log('order item: ');
+          orderItem.quantity = quantity;
+          console.log(orderItem);
+
+          this.orderService.updateOrderItem(this.sessionService.UserToken, orderItem)
+            .subscribe( response => {
+              console.log('updated response: ');
+              console.log(response);
+
+              this.messageService.setCartItem(response);
+            },  
+            (error: string) => {
+              console.log(error);
+              this.messageService.setSpinner(false);
+            });
+      },  
+      (error: string) => {
+        console.log(error);
+        this.messageService.setSpinner(false);
+      });
+  }
+  
 }
