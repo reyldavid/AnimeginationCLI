cbf1201d378b572f2256e19d28c67ecd8326796c
Rey David <reydavid47@gmail.com>
2019-10-11 11:43:07 -0700

    New ORDER ITEM

---
+++ src/app/order-item/order-item.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product, ApiProduct } from '../models/product';
+import { OrderItem } from '../models/orderItemModel';
+import { ProductsService } from '../services/products.service';
+import { MessageService } from '../services/message.service';
+
+@Component({
+  selector: 'app-order-item',
+  templateUrl: './order-item.component.html',
+  styleUrls: ['./order-item.component.css']
+})
+export class OrderItemComponent implements OnInit {
+
+  @Input() set orderItem(orderItem: OrderItem) {
+    console.log("order Item ", orderItem);
+    this.GetProduct(orderItem);
+    this.quantity = orderItem.Quantity;
+  }
+
+  product: ApiProduct;
+  quantity: number;
+  show: boolean = false;
+
+  constructor( private router: Router, 
+    private productService: ProductsService, 
+    private messageService: MessageService ) { 
+  }
+
+  ngOnInit() {
+  }
+
+  GetProduct(orderItem: OrderItem) {
+    this.productService.getAnimeProduct(orderItem.ProductID)
+      .subscribe(((product: ApiProduct) => 
+        {
+          this.product = product;
+          this.show = true;
+
+          this.messageService.setSpinner(false);
+        }
+    ));
+  }
+
+  OnSelectProduct(orderItem: OrderItem) {
+    console.log('product ID: ' + orderItem.ProductID);
+    this.router.navigate(['/detail'], { queryParams: {  productID: orderItem.ProductID } });
+  }
+
+  BuyAgain(product: Product) {
+    console.log('product ID: ', product);
+  }
+
+  WriteReview(product: Product) {
+    console.log('product ID: ', product);
+  }
+}
