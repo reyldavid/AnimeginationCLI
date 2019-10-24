c66d06d07934211b40f82706c98431542a379550
Rey David <reydavid47@gmail.com>
2019-10-23 22:18:01 -0700

    Genre Edit component to Admin module

---
+++ src/app/admin/genre-edit/genre-edit.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GenreEditComponent } from './genre-edit.component';
+
+describe('GenreEditComponent', () => {
+  let component: GenreEditComponent;
+  let fixture: ComponentFixture<GenreEditComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GenreEditComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GenreEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
