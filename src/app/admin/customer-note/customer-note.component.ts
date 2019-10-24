f75f3dd498e93ca8b37b1a7fc7485e7d93b05b54
Rey David <reydavid47@gmail.com>
2019-10-23 22:17:28 -0700

    Customer Note component to Admin module

---
+++ src/app/admin/customer-note/customer-note.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-note',
+  templateUrl: './customer-note.component.html',
+  styleUrls: ['./customer-note.component.css']
+})
+export class CustomerNoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
