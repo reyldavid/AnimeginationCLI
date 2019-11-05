ae7b44cf65d8f00ed2e27b4d36d8fec4c0751ed2
Rey David <reydavid47@gmail.com>
2019-11-04 19:45:49 -0800

    New Listings component

---
+++ src/app/admin/listing-add/listing-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-add',
+  templateUrl: './listing-add.component.html',
+  styleUrls: ['./listing-add.component.css']
+})
+export class ListingAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
