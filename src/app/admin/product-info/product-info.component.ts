1c9c1901e5d23e9b3edbc2d51b7f3d7208c2f365
Rey David <reydavid47@gmail.com>
2019-11-04 19:47:12 -0800

    New PRODUCT INFO component

--- src/app/admin/product-info/product-info.component.ts
+++ src/app/admin/product-info/product-info.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+import { ApiProduct, Product } from '../../models/product';
+import { Router } from '@angular/router';
+import { SessionService } from '../../services/session.service';
+import { TokenModel } from '../../models/tokenmodel';
+import { MessageService } from '../../services/message.service';
 
 @Component({
   selector: 'app-product-info',
@@ -7,9 +13,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductInfoComponent implements OnInit {
 
-  constructor() { }
+  products: ApiProduct[] = [];
+
+  constructor( private router: Router,
+               private productsService: ProductsService,  
+               private sessionService: SessionService, 
+               private messageService: MessageService ) { }
 
   ngOnInit() {
+    this.productsService.getAnimeProducts().subscribe(products => {
+      this.products = products;
+      this.productsService.setProductsCache(products);
+      this.messageService.setSpinner(false);
+    })
+  }
+
+  OnEditProduct(product: ApiProduct) {
+    console.log('product ID: ' + product.ProductID);
+    this.router.navigate(['/product-edit'], { queryParams: {  productID: product.ProductID } });
+  }
+
+  OnDeleteProduct(product: ApiProduct) {
+    if (this.sessionService.isAuthenticated()) {
+      let token: TokenModel = this.sessionService.UserToken;
+
+      this.productsService.deleteProduct(token, product.ProductID).subscribe(item => {
+        this.messageService.setSpinner(false);
+        console.log("aya deleted product ", item);
+        this.router.navigate(['/product-info']);
+      })
+    }
   }
 
 }
+
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -11,13 +11,21 @@ import { Globals } from "../globals";
 import { HttpHelper } from './http.helper.service';
 import { environment } from '../../environments/environment';
 import { ServiceName } from '../models/service';
-import { ApiProduct } from '../models/product';
+import { ApiProduct, Product } from '../models/product';
 import { TokenModel } from '../models/tokenmodel';
 import { ApiProductCache, ApiProductsCache } from '../models/dictionary';
 import 'rxjs/Rx';
 // import { Subscription } from 'rxjs/Subscription';
 import { MessageService } from '../services/message.service';
+import { MediaService } from '../services/media.service';
+import { CategoryService } from '../services/categories.service';
+import { PublishersService } from '../services/publishers.service';
 //import 'rxjs/add/operator/map';
+import * as _ from 'lodash';
+import { Medium } from '../models/medium';
+import { Category } from '../models/category';
+import { Publisher } from '../models/publisher';
+import { reject } from 'q';
 
 @Injectable({
     providedIn: 'root'
@@ -32,7 +40,10 @@ import { MessageService } from '../services/message.service';
     constructor(private http: HttpClient, 
         private globals: Globals, 
         private helper: HttpHelper, 
-        private messageService: MessageService ) {
+        private messageService: MessageService,
+        private genreService: CategoryService,
+        private mediaService: MediaService,
+        private publisherService: PublishersService ) {
     }
 
     setProductsCache(data: ApiProduct[]) {
@@ -129,4 +140,145 @@ import { MessageService } from '../services/message.service';
         }
 
     }
+
+  updateProduct(token: TokenModel, product: Product): Observable<Product> {
+
+      if (this.globals.localData) {
+      }
+      else {
+          this.messageService.setSpinner(true);
+          let body = JSON.stringify(product);
+
+          let endpoint = this.helper.getEndPoint(ServiceName.product);
+
+          let headers: HttpHeaders = this.helper.getSecureContentHeaders(token);
+
+          let observables = this.http.put<Product>(
+                  endpoint, body, 
+                  { headers: headers, observe: 'response'} )
+              .pipe( map ( HttpHelper.extractData), catchError( HttpHelper.handleError ));
+
+          return observables;
+      }
+  }
+
+  addProduct(token: TokenModel, productItem: Product): Observable<Product> {
+
+    if (this.globals.localData) {
+    }
+    else {
+        this.messageService.setSpinner(true);
+        let endpoint = this.helper.getEndPoint(ServiceName.product);
+
+        let headers: HttpHeaders = this.helper.getSecureContentHeaders(token);
+
+        let body = JSON.stringify(productItem);
+
+        let observables = this.http.post<ApiProduct>(
+            endpoint, body, { headers: headers, observe: 'response'}
+            )
+            .pipe( map ( HttpHelper.extractData), catchError( HttpHelper.handleError ));
+
+        return observables;
+    }
+  }
+
+  deleteProduct(token: TokenModel, id: number): Observable<Product> {
+
+    if (this.globals.localData) {
+    }
+    else {
+        this.messageService.setSpinner(true);
+        let endpoint = this.helper.getEndPoint(ServiceName.product, id);
+
+        let headers: HttpHeaders = this.helper.getSecureContentHeaders(token);
+
+        let observables = this.http.delete<ApiProduct>(
+                endpoint, 
+                { headers: headers, observe: 'response'} )
+            .pipe( map ( HttpHelper.extractData), catchError( HttpHelper.handleError ));
+
+        return observables;
+    }
+  }
+
+  convertProduct(apiProduct: ApiProduct): Product {
+
+    let medium: Medium = _.find(this.mediaService.Media, function(m) {
+        return m.MediumName == apiProduct.MediumName;
+    })
+
+    let genre: Category = _.find(this.genreService.Categories, function(g) {
+        return g.CategoryName == apiProduct.CategoryName;
+    })
+
+    let publisher: Publisher = _.find(this.publisherService.Publishers, function(p) {
+        return p.PublisherName == apiProduct.PublisherName;
+    })
+
+    let product: Product = {
+        productid: apiProduct.ProductID,
+        productcode: apiProduct.ProductCode,
+        producttitle: apiProduct.ProductTitle,
+        productdescription: apiProduct.ProductDescription,
+        unitprice: apiProduct.UnitPrice,
+        yourprice: apiProduct.YourPrice,
+        categoryid: genre.CategoryID,
+        productagerating: apiProduct.ProductAgeRating,
+        productlength: apiProduct.ProductLength,
+        productyearcreated: apiProduct.ProductYearCreated,
+        mediumid: medium.MediumID,
+        publisherid: publisher.PublisherID,
+        ratingid: apiProduct.RatingID
+    }
+    return product;
+
+    // const promiseMedium = new Promise<Medium>((resolve, reject) => {
+    //     let medium: Medium = _.find(this.mediaService.Media, function(m) {
+    //         return m.MediumName == apiProduct.MediumName;
+    //     })
+    //     resolve(medium);  
+    // })
+
+    // const promiseGenre = new Promise<Category>((resolve, reject) => {
+    //     let genre: Category = _.find(this.genreService.Categories, function(g) {
+    //         return g.CategoryName == apiProduct.CategoryName;
+    //     })
+    //     resolve(genre);
+    // })
+
+    // const promisePublisher = new Promise<Publisher>((resolve, reject) => {
+    //     let publisher: Publisher = _.find(this.publisherService.Publishers, function(p) {
+    //         return p.PublisherName == apiProduct.PublisherName;
+    //     })
+    // })
+
+    // promiseMedium.then(medium => {
+    //     promiseGenre.then(genre => {
+    //         promisePublisher.then(publisher => {
+
+    //             let product: Product = {
+    //                 productid: apiProduct.ProductID,
+    //                 productcode: apiProduct.ProductCode,
+    //                 producttitle: apiProduct.ProductTitle,
+    //                 productdescription: apiProduct.ProductDescription,
+    //                 unitprice: apiProduct.UnitPrice,
+    //                 yourprice: apiProduct.YourPrice,
+    //                 categoryid: genre.CategoryID,
+    //                 productagerating: apiProduct.ProductAgeRating,
+    //                 productlength: apiProduct.ProductLength,
+    //                 productyearcreated: apiProduct.ProductYearCreated,
+    //                 mediumid: medium.MediumID,
+    //                 publisherid: publisher.PublisherID,
+    //                 ratingid: apiProduct.RatingID
+    //             }
+    //             console.log("aya pr 1 ", product);
+    //             return product;
+    //         })
+    //     }).then(pr => {console.log("aya pr 2", pr); return pr})
+    // }).then(pr => {console.log("aya pr 3", pr); return pr});
+
+    // return null;
+  }
+
 }
