fa85f47c74733540fa1275c30a67a1632d105405
Rey David <reydavid47@gmail.com>
2019-11-05 18:10:21 -0800

    New LIST TYPE component

---
+++ src/app/admin/list-type/list-type.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ListTypeService } from '../../services/listtypes.service';
+import { ListType } from '../../models/listtype';
+import { Router } from '@angular/router';
+import { SessionService } from '../../services/session.service';
+import { MessageService } from '../../services/message.service';
+
+@Component({
+  selector: 'app-list-type',
+  templateUrl: './list-type.component.html',
+  styleUrls: ['./list-type.component.css']
+})
+export class ListTypeComponent implements OnInit {
+
+  listTypes: ListType[] = [];
+
+  constructor( private router: Router,
+               private listTypesService: ListTypeService, 
+               private messageService: MessageService ) { }
+
+  ngOnInit() {
+    this.listTypesService.getAnimeListTypes().subscribe(listTypes => {
+      this.listTypes = listTypes;
+      this.listTypesService.setListTypesCache(listTypes);
+    })
+  }
+
+  OnEditListType(listType: ListType) {
+    console.log('listType ID: ' + listType.ListTypeID);
+    this.router.navigate(['/list-type-edit'], { queryParams: {  listTypeID: listType.ListTypeID } });
+  }
+
+}
+
+
--- src/app/services/listtypes.service.ts
+++ src/app/services/listtypes.service.ts
@@ -23,11 +23,26 @@ import 'rxjs/Rx';
   export class ListTypeService {
 
     private _animeListType = new ListTypeCache();
+    private _listTypes: ListType[] = []; 
 
     constructor(private http: HttpClient, 
         private globals: Globals, 
         private helper: HttpHelper) {
         // private messageService: MessageService) {
+    }
+
+    get ListTypes(): ListType[] {
+        return this._listTypes;
+    }
+    set ListTypes(listTypes: ListType[]) {
+        this._listTypes = listTypes;
+    }
+
+    setListTypesCache(data: ListType[]) {
+        if (!this._animeListType[0]) {
+        this._animeListType[0] = data;
+        this._listTypes = data;
+        }
     }
 
     setListTypeCache(data: ListType, id: number) {
@@ -45,6 +60,34 @@ import 'rxjs/Rx';
         .pipe( catchError( HttpHelper.handleError ));
 
         return result;
+    }
+
+    getAnimeListTypes(): Observable<ListType[]> {
+
+        if (this.globals.localData) {
+            // return this.getAnimeListTypeStatic()[0];
+            return of(null);
+        }
+        else {
+            if (this._listTypes && this._listTypes.length > 0) {
+
+                console.log('aya Anime List Type cached');
+                
+                return of(this._listTypes);
+            }
+            else {
+                // this.messageService.setSpinner(true);                
+                let endpoint = this.helper.getEndPoint(ServiceName.listType);
+                let headers: HttpHeaders = this.helper.getContentHeaders();
+    
+                let observables = this.http.get<ListType[]>(
+                    endpoint, { headers: headers, observe: 'response'}
+                    )
+                    .pipe( map ( HttpHelper.extractData), catchError( HttpHelper.handleError ));
+
+                return observables;
+            }
+        }
     }
 
     getAnimeListType(listTypeID: number): Observable<ListType> {
