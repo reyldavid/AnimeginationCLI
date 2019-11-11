ca56480f48400fcfa3f53417589710e50af6947c
Rey David <reydavid47@gmail.com>
2019-11-10 19:11:16 -0800

    LISTING component

--- src/app/admin/listing/listing.component.ts
+++ src/app/admin/listing/listing.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ListingService } from '../../services/listings.service';
+import { Listing } from '../../models/listing';
+import { Router } from '@angular/router';
+import { SessionService } from '../../services/session.service';
+import { TokenModel } from '../../models/tokenmodel';
+import { MessageService } from '../../services/message.service';
 
 @Component({
   selector: 'app-listing',
@@ -7,9 +13,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingComponent implements OnInit {
 
-  constructor() { }
+  listings: Listing[] = [];
+
+  constructor( private router: Router,
+               private listingsService: ListingService, 
+               private sessionService: SessionService, 
+               private messageService: MessageService ) { }
 
   ngOnInit() {
+    this.listingsService.getAnimeListings().subscribe(listings => {
+      this.listings = listings;
+      this.listingsService.Listings = listings;
+    })
+  }
+
+  OnEditListing(listing: Listing) {
+    console.log('listing ID: ' + listing.ListingID);
+    this.router.navigate(['/listing-edit'], { queryParams: {  listingID: listing.ListingID } });
+  }
+
+  OnDeleteListing(listing: Listing) {
+    if (this.sessionService.isAuthenticated()) {
+      let token: TokenModel = this.sessionService.UserToken;
+
+      this.listingsService.deleteListing(token, listing).subscribe(item => {
+        this.messageService.setSpinner(false);
+        console.log("aya deleted listing ", item);
+      })
+    }
   }
 
 }
+
+
